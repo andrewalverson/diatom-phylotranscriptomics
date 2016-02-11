0a53875d4fbb833f3c376f8e416c9eaf8da78f10
andrewalverson <andy.alverson@gmail.com>
2016-02-11 15:51:20 -0600

    added full set of options to parseArgs(), including rcorrector and transrate options

--- write_rnaseq_clean_filter_jobscript.pl
+++ write_rnaseq_clean_filter_jobscript.pl
@@ -4,14 +4,25 @@ use warnings;
 use strict;
 use Getopt::Long;
 
+# settings for this script
 my $SCRIPT_WALL   = 18;    # walltime for running 'rnaseq_clean_filter.pl'
 my $SCRIPT_QUEUE  = 'aja'; # queue for Trinity PBS script
-my $TRINITY_WALL  = 12;    # walltime for Trinity, which will be written to the Trinity PBS job script
-my $TRINITY_QUEUE = 'mem512GB64core'; # queue for Trinity PBS script
-my $MIN_KMER_RNA   = 1;     # for Trinity, min_kmer_cov parameter (rRNA assembly)
-my $MIN_KMER_ORG   = 1;     # for Trinity, min_kmer_cov parameter (organelle assembly)
-my $MIN_KMER_NUC   = 2;     # for Trinity, min_kmer_cov parameter (nuclear assembly)
+
+# settings for writing Trinity job script within 'rnaseq_clean_filter.pl'
+my $TRINITY_QUEUE  = 'mem512GB64core'; # queue for Trinity PBS script
+my $TRINITY_WALL   = 12; # walltime for Trinity, which will be written to the Trinity PBS job script
+my $MIN_KMER_RNA   = 1;  # for Trinity, min_kmer_cov parameter (rRNA assembly)
+my $MIN_KMER_ORG   = 1;  # for Trinity, min_kmer_cov parameter (organelle assembly)
+my $MIN_KMER_NUC   = 1;  # for Trinity, min_kmer_cov parameter (nuclear assembly)
+
+# whether to normalize with BBNORM while running 'rnaseq_clean_filter.pl'
 my $NORMALIZE;
+
+# settings for running Trimmomatic within 'rnaseq_clean_filter.pl'
+my $SLIDINGWINDOW = '4:5'; # Trimmomatic SLIDINGWINDOW paramter - number of 5' bp to trim
+my $TRIM_PHRED    =  2;    # Trimmomatic LEADING and TRAILING partameters - bases below this Phred score will be trimmed from 5' and 3' ends of reads
+my $MIN_LEN       = 25;    # Trimmomatic MINLEN parameter - remove reads shorter than this
+
 
 parseArgs();
 
@@ -69,7 +80,9 @@ print PBS 'module load bowtie', "\n";
 print PBS 'module load samtools/0.1.19', "\n";
 print PBS "module load bbmap\n\n";
 
-print PBS "/home/aja/local/src/scripts/rnaseq_clean_filter.pl $rnaID --wall=$TRINITY_WALL --queue=$TRINITY_QUEUE $NORMALIZE --min_kmer_rna=$MIN_KMER_RNA --min_kmer_org=$MIN_KMER_ORG --min_kmer_nuc=$MIN_KMER_NUC\n";
+print PBS "/home/aja/local/src/scripts/rnaseq_clean_filter.pl $rnaID --wall=$TRINITY_WALL --queue=$TRINITY_QUEUE $NORMALIZE \
+          --min_kmer_rna=$MIN_KMER_RNA --min_kmer_org=$MIN_KMER_ORG --min_kmer_nuc=$MIN_KMER_NUC
+          --window=$SLIDINGWINDOW --trim_phred=$TRIM_PHRED --min_len=$MIN_LEN\n";
 
 close PBS;
 
@@ -80,17 +93,29 @@ sub parseArgs{
 
   my $usage = "\nUsage: $0 RNA_ID [options]
 
-   options for running this script
+
+   options for running the 'rnaseq_clean_filter.pl' pipeline
           --script_wall   - walltime for running 'rnaseq_clean_filter.pl' (integer, default = 12 hrs)
           --script_queue  - queue for running 'rnaseq_clean_filter.pl' ('aja' [default], 'med12core', 'med16core')
+
+
+   option to normalize within 'rnaseq_clean_filter.pl' pipeline
           --normalize     - perform kmer normalization with BBNorm (default: false)
 
-   options for Trinity run
+
+   options for running Trimmomatic within 'rnaseq_clean_filter.pl' pipeline
+          --window     - Trimmomatic SLIDINGWINDOW parameter specified as <windowSize><requiredQuality> (default: '4:5')
+          --trim_phred - Trimmomatic LEADING and TRAILING partameters; bases below this minimum Phred quality
+                             will be trimmed from 5' and 3' ends (default: 2)
+          --min_len    - filter reads shorter than this length threshold (default: 25)
+
+
+   options for writing Trinity PBS job script within 'rnaseq_clean_filter.pl' pipeline
           --trinity_wall  - walltime for Trinity PBS script (integer, default = 12 hrs)
           --trinity_queue - queue for Trinity PBS script ('mem256GB48core', 'mem512GB64core' [default], 'mem768GB32core', 'random')
           --min_kmer_rna   - Trinity min_kmer_cov parameter for rRNA assembly (default: 1)
           --min_kmer_org   - Trinity min_kmer_cov parameter for organelle assembly (default: 1)
-          --min_kmer_nuc   - Trinity min_kmer_cov parameter for nuclear assembly (default: 2)
+          --min_kmer_nuc   - Trinity min_kmer_cov parameter for nuclear assembly (default: 1)
 
 \n";
 
@@ -98,9 +123,12 @@ sub parseArgs{
 	(
 	 'script_wall=i'   => \$SCRIPT_WALL,
 	 'script_queue=s'  => \$SCRIPT_QUEUE,
+	 'normalize!'      => \$NORMALIZE,
+	 'window=s'        => \$SLIDINGWINDOW,
+	 'trim_phred=s'    => \$TRIM_PHRED,
+	 'min_len=s'       => \$MIN_LEN,
 	 'trinity_wall=i'  => \$TRINITY_WALL,
 	 'trinity_queue=s' => \$TRINITY_QUEUE,
-	 'normalize!'      => \$NORMALIZE,
 	 'min_kmer_rna=i'  => \$MIN_KMER_RNA,
 	 'min_kmer_org=i'  => \$MIN_KMER_ORG,
 	 'min_kmer_nuc=i'  => \$MIN_KMER_NUC,
