4d7d510402a730c215d845ca895cddc44dc034e4
andrewalverson <andy.alverson@gmail.com>
2018-11-01 15:13:29 -0500

    minor fixes

--- filter_crosstalk.py
+++ filter_crosstalk.py
@@ -10,9 +10,19 @@
 import sys
 import os
 import re
+import argparse
 from Bio import SeqIO
 
-min_seq_length = 300
+# create an ArgumentParser object ('parser') that will hold all the information necessary to parse the command line
+parser = argparse.ArgumentParser()
+
+# add arguments
+parser.add_argument( "type", help="The type of assembly - rna or [nuc]lear" )
+parser.add_argument( "src",  help="Data source - alverson lab <alv> or other <oth>" )
+parser.add_argument( "--min_seq_length", help="Filter sequences < min_length in length", default=300, type=int )
+parser.add_argument( "--subseq_identity_threshold", help="Sequence identity threshold for CD-HIT", default=1, type=float )
+
+args = parser.parse_args()
 
 # get current working directory
 cwd = os.getcwd()
@@ -25,8 +35,11 @@ for file1 in directory_listing:
     from collections import defaultdict
     # delete_seqs = defaultdict(dict)
     delete_seqs = defaultdict(list)
-    filename_re = '([RL]\d+)(_.*)\.fa$' # e.g., R25_ssu_diatom_rrna.fa
-   
+    if args.src == 'alv':
+        filename_re = '([RL]\d+)(_.*)\.fa$' # e.g., R25_ssu_diatom_rrna.fa
+    else:
+        filename_re = '(.*)\.fa$' # *.fa
+
     if re.search( filename_re, file1 ):
         for file2 in directory_listing:
             if re.search( filename_re, file2 ) and ( file1 != file2 ):
@@ -35,7 +48,8 @@ for file1 in directory_listing:
                 outfile = outfile_pt1 + '-' + outfile_pt2
 
                 # generate and run CD-HIT-EST-2D command (nucleotide sequences)
-                cd_hit = "cd-hit-est-2d" + " -c 1" + " -T 0" + " -d 300" + " -aS 1" + " -n 10" + " -p 1" + " -i " + file2 + " -i2 " + file1 + " -o " + outfile + "> /dev/null"
+                cd_hit = "cd-hit-est-2d" + " -c " + str(args.subseq_identity_threshold) + " -T 0" + " -d 99999" + " -aS 1" + " -n 10" + " -p 1" + " -i " + file2 + " -i2 " + file1 + " -o " + outfile + "> /dev/null"
+                print( outfile )
                 os.system( cd_hit )
                 os.remove( outfile )
 
@@ -49,6 +63,7 @@ for file1 in directory_listing:
                         f = line.split()
                         delete_seqs[file1].append(f[2][1:-3])
                 clstr_file.close()
+                os.remove( outfile + ".clstr" )
 
     else:
         continue
@@ -64,11 +79,11 @@ for file1 in directory_listing:
     # open output file
     with open( filtered_fasta, "w" ) as f:
         for acc in fasta_sequences:
-            if acc.id not in delete_seqs[file1] and len(acc.seq) > min_seq_length:
-                SeqIO.write( [acc], f, "fasta" )
+            if acc.id not in delete_seqs[file1] and len(acc.seq) > args.min_seq_length:
+                SeqIO.write( [acc], "fasta" )
 
 
-# the *.clstr files, which are formatted like so:
+# the *.clstr files, which are formatted like this:
 
 # >Cluster 0
 # 0	1980nt, >TRINITY_DN1_c0_g1_i1_R15_Attheya_longicornis... *
