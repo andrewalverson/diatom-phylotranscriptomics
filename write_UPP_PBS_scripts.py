4ebac71b079d059b53328edaefd9c4f52980128e
andrewalverson <andy.alverson@gmail.com>
2019-01-24 14:29:06 -0600

    compute alignment statistics for setting '-M' option in UPP

--- write_UPP_PBS_scripts.py
+++ write_UPP_PBS_scripts.py
@@ -4,6 +4,8 @@ import sys
 import os
 import re
 import argparse
+import statistics
+from Bio import SeqIO
 
 # create an ArgumentParser object ('parser') that will hold all the information necessary to parse the command line
 parser = argparse.ArgumentParser()
@@ -13,8 +15,31 @@ parser.add_argument( "fasta", help="Unaligned FASTA file" )
 
 args = parser.parse_args()
 
+### compute median sequence length for this dataset ###
+
+lengths = [] # list of sequence lengths
+upper_half_lengths = [] # list of sequences lengths >= median sequence length
+median_length = 0 # store median sequence length
+upper_median  = 0 # calculate the median length for all sequence, then calculate the median for all sequences >= the global median length
+
+# open and parse FASTA file, storing sequence lengths
+fasta_sequences = SeqIO.parse(open(args.fasta),'fasta')
+for record in fasta_sequences:
+    lengths.append(len(record.seq))
+fasta_sequences.close()
+
+median_length = int(statistics.median(lengths))
+
+for i in lengths:
+	if i >= median_length:
+		upper_half_lengths.append(i)
+
+upper_median = int(statistics.median(upper_half_lengths))
+
+### write UPP PBS script for this dataset ###
+
 # set walltime and number of cpu's
-wall    = 6
+wall    = 72
 num_cpu = 32
 
 # set input directory
@@ -29,7 +54,7 @@ prefix = a[0]
 
 # print top of PBS script
 print('#PBS -N', prefix+'.UPP')
-print('#PBS -q q06h32c')
+print('#PBS -q q72h32c')
 print('#PBS -j oe')
 print('#PBS -o',  prefix+'.UPP.$PBS_JOBID')
 print('#PBS -l nodes=1:ppn=32')
@@ -44,5 +69,5 @@ print('module load gcc/7.2.1 python/3.6.0 java/sunjdk_1.8.0 sepp/4.3.5')
 print()
 
 print('# run UPP')
-print('run_upp.py', '-m amino', '-x', str(num_cpu), '-s', '/'.join([indir, args.fasta]), '-d', outdir, '-o', prefix)
+print('run_upp.py', '-m amino', '-x', str(num_cpu), '-s', '/'.join([indir, args.fasta]), '-d', outdir, '-o', prefix, '-M', upper_median)
 
